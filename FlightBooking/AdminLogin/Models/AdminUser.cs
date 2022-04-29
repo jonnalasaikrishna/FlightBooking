9cb13a62f138a56b6410d6bd904a5dce8696aa4b
unknown <cogdotnet15@iiht.tech>
2022-04-29 17:49:35 +0530

    AdminUsers

---
+++ FlightBooking/AdminLogin/Models/AdminUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace AdminLogin.Models
+{
+    public partial class AdminUser
+    {
+        public int Id { get; set; }
+        public int? AdminId { get; set; }
+        public string Password { get; set; }
+        public string AdminName { get; set; }
+    }
+}
